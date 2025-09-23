789d0639f16d44f182f046fa4a836ea33ab42c6d
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-09-23 14:25:45 +0800

    add `nuitka` build script for windows

---
+++ build.nuitka.bat
@@ -0,0 +1,6 @@
+@echo off
+
+uv sync
+uv pip install nuitka
+call .venv\Scripts\activate.bat
+nuitka --standalone --onefile --main=tui.py --output-filename=PKU_elective_prettify.exe --deployment --clang --lto=yes --windows-icon-from-ico=assets.build\PKU.icon.ico --company-name=ParkSnoopyInc --product-name=PKU_elective_prettify --file-version=0.2.0.0 --product-version=0.2.0.0
