47657b860f4a0dbdc185cdb70d5583b75345456a
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2025-10-15 09:18:51 +0800

    update build script
    - bump version `0.2.0.0` -> `0.2.1.0`
    - replace `activate` and `deactivate` by `uv run --`

--- build.nuitka.bat
+++ build.nuitka.bat
@@ -2,9 +2,6 @@
 
 uv sync
 uv pip install nuitka
-
-call .venv\Scripts\activate.bat
-nuitka --standalone --onefile --main=tui.py --output-filename=output\PKU_elective_prettify.exe --deployment --clang --lto=yes --windows-icon-from-ico=assets.build\PKU.icon.ico --company-name=ParkSnoopyInc --product-name=PKU_elective_prettify --file-version=0.2.0.0 --product-version=0.2.0.0
-deactivate
+uv run -- nuitka --standalone --onefile --main=tui.py --output-filename=output\PKU_elective_prettify.exe --deployment --clang --lto=yes --windows-icon-from-ico=assets.build\PKU.icon.ico --company-name=ParkSnoopyInc --product-name=PKU_elective_prettify --file-version=0.2.1.0 --product-version=0.2.1.0
 
 PAUSE
