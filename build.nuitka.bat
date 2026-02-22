4d6c30d1b2c77e4856d41f6bbb924b6ea4de8472
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-02-23 07:54:48 +0800

    Remove: nuitka build script
    Since `Spire.XLS` is somewhat proprietary,
    Unable to stably include dependencies.

--- build.nuitka.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-uv sync
-uv pip install nuitka
-uv run -- nuitka --standalone --onefile --main=cli.py --output-filename=output\PKU_elective_prettify.exe --deployment --clang --lto=yes --windows-icon-from-ico=assets.build\PKU.icon.ico --company-name=ParkSnoopyInc --product-name=PKU_elective_prettify --file-version=0.2.1.0 --product-version=0.2.1.0
-
-PAUSE
