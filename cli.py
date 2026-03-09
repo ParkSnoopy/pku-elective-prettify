e9470ef5cabef4bdd5dd5b9439970afd974fc623
ParkSnoopy <ParkSnoopy@users.noreply.github.com>
2026-03-09 14:00:15 +0800

    update: put everything on name_main block

--- cli.py
+++ cli.py
@@ -9,49 +9,49 @@ from cli_utils import (
 
 import os
 import questionary
-# import excel3img
 
 
 
-check_schedule_download()
+if __name__ == "__main__":
+    check_schedule_download()
 
-i_filepath = get_schedule_filepath()
-i_filename = i_filepath.name.split('.')[0]
-o_filepath = get_output_filepath()
-formats = get_output_formats()
+    i_filepath = get_schedule_filepath()
+    i_filename = i_filepath.name.split('.')[0]
+    o_filepath = get_output_filepath()
+    formats = get_output_formats()
 
-ct = CourseTable(i_filepath)
+    ct = CourseTable(i_filepath)
 
-_re_run = True
-while _re_run:
-    _xlsx_path = o_filepath / f"{i_filename}.xlsx"
-    _png_path  = o_filepath / f"{i_filename}.png"
+    _re_run = True
+    while _re_run:
+        _xlsx_path = o_filepath / f"{i_filename}.xlsx"
+        _png_path  = o_filepath / f"{i_filename}.png"
 
-    ct.prepare()
-    ct.export(_xlsx_path)
+        ct.prepare()
+        ct.export(_xlsx_path)
 
-    if "png" in formats:
-        if "nt" != os.name.lower():
-            print("  [ ERR ] Export to PNG is only supported on Windows.")
-            break
+        if "png" in formats:
+            if "nt" != os.name.lower():
+                print("  [ ERR ] Export to PNG is only supported on Windows.")
+                break
 
-        save_png(
-            xlsx_path=_xlsx_path,
-            png_path=_png_path,
-        )
+            save_png(
+                xlsx_path=_xlsx_path,
+                png_path=_png_path,
+            )
 
-        if questionary.confirm(
-            "Check for generated PNG file",
-            default=True,
-        ).ask():
-            os.startfile("{}".format(str(_png_path).replace("/", "\\")))
+            if questionary.confirm(
+                "Check for generated PNG file",
+                default=True,
+            ).ask():
+                os.startfile("{}".format(str(_png_path).replace("/", "\\")))
 
-    if "xlsx" not in formats:
-        os.remove(_xlsx_path)
+        if "xlsx" not in formats:
+            os.remove(_xlsx_path)
 
-    _re_run = questionary.confirm(
-        "Re-Run to generate a new one",
-        default=False,
-    ).ask()
+        _re_run = questionary.confirm(
+            "Re-Run to generate a new one",
+            default=False,
+        ).ask()
 
-    print()
+        print()
